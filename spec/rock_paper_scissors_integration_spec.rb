0aa0b24bb3a5eb6f559e7a57a4eefc4b76c33de4
Guest User <student@epicodus.com>
2015-08-05 16:37:12 -0700

    fix integration specs and make sure app works

--- spec/rock_paper_scissors_integration_spec.rb
+++ spec/rock_paper_scissors_integration_spec.rb
@@ -8,6 +8,7 @@ describe("the result path", {:type => :feature}) do
     visit('/')
     fill_in('Player1', :with => 'rock')
     fill_in('Player2', :with => 'paper')
+
     click_button('Go!')
     expect(page).to have_content("Player 2 wins!")
   end
