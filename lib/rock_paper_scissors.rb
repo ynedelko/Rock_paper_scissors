89df54449f568fe23a9989f19d63b458438164d2
Guest User <student@epicodus.com>
2015-08-05 14:33:15 -0700

    Handles cases where object or argument is not recognized

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,6 +1,7 @@
 class String
   #def beats?(challenger)
   define_method(:beats?) do |challenger|
+    #unrecognized = "I have no idea what you're doing, try again!"
     downcase!
     challenger.downcase!
     if self == "rock"
@@ -10,6 +11,8 @@ class String
         false
       elsif challenger == "rock"
         nil
+      else
+        "unrecognized"
       end
     elsif self == "paper"
       if challenger == "scissors"
@@ -18,15 +21,21 @@ class String
         true
       elsif challenger == "paper"
         nil
+      else
+        "unrecognized"
       end
     elsif self == "scissors"
-        if challenger == "rock"
-          false
-        elsif challenger == "paper"
-          true
-        elsif challenger == "scissors"
-          nil
-        end
+      if challenger == "rock"
+        false
+      elsif challenger == "paper"
+        true
+      elsif challenger == "scissors"
+        nil
+      else
+        "unrecognized"
+      end
+    else
+      "unrecognized"
     end
   end
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -29,4 +29,8 @@ describe('String#beats?') do
   it("returns nil if scissors is the object and scissors is the argument") do
     expect("scissors".beats?("scissors")).to(eq(nil))
   end
+  it("returns a message letting user know if object or argument was not recognized") do
+    expect("scisors".beats?("rock")).to(eq("unrecognized"))
+    expect("scissors".beats?("rocc")).to(eq("unrecognized"))
+  end
 end
