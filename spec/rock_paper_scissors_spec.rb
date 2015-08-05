7d6f9f2ec6d53d27b40d6ae92d2f8d3bdbe17c04
Guest User <student@epicodus.com>
2015-08-05 13:45:46 -0700

    Create 1st spec handeling rock vs scissors

---
+++ spec/rock_paper_scissors_spec.rb
@@ -0,0 +1,7 @@
+require('rspec')
+require('rock_paper_scissors')
+
+describe('String#beats') do
+  it("returns true if rock is the object and scissors is the argument") do
+    expect("rock".beats?("scissors")).to(eq(true))
+  end
